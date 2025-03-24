dbf14a60f6d1367f51302a717589ead238fadc92
Eliott-Guignabaudet <eliott.guignabaudet@gmail.com>
2025-03-24 18:52:01 +0100

    Add player controller and projectile interface c++

---
+++ Source/FurieuxOiseaux/ProjectileInterface.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ProjectileInterface.generated.h"
+
+/**
+ * 
+ */
+UINTERFACE(MinimalAPI, Blueprintable)
+class UMyInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class IMyInterface
+{
+	GENERATED_BODY()
+
+public:
+	
+	
+};
+
